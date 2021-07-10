a67fd10c5e7f692cf285209379e9d873fcbb5980
Nathaniel Thomas <nthomas20@gmail.com>
2021-07-10 11:02:55 -0500

    re-enable gzip compression

--- app/api/api.go
+++ app/api/api.go
@@ -103,8 +103,7 @@ func (config *Configuration) Run() bool {
 	// Launch our listener!
 	go func() {
 		// Trigger compression handler
-		// handler := fasthttp.CompressHandler(r.Handler)
-		handler := r.Handler
+		handler := fasthttp.CompressHandler(r.Handler)
 
 		ln, err := reuseport.Listen("tcp4", ":"+config.Configuration.Port)
 		if err != nil {
